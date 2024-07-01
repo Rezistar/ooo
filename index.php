664c93e4e845ac052b8c85c65910c7993509f7d5
Rezistar <130656413+Rezistar@users.noreply.github.com>
2024-07-01 19:34:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IP Logger</title>
+</head>
+<body>
+  <?php
 $user_ip = $_SERVER['REMOTE_ADDR'];
 $file = 'ips.txt';
 $current = file_get_contents($file);
@@ -7,3 +15,8 @@ file_put_contents($file, $current);
 header('Location: index.html');
 exit;
 ?>
+    <h1>Hello world</h1>
+</html>
+
+
+
